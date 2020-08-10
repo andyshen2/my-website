d29ef6fccf45287f42781d7b76687d2909a72ced
andyshen2 <andyshen96@gmail.com>
2020-08-10 13:29:39 +1200

    functional components

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,37 +1,32 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./NavBar.css";
-import { StickyContainer, Sticky } from "react-sticky";
-// const ref = React.createRef();
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { about: true };
+// class NavBar extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-    // this.handleClick = this.handleClick.bind(this);
-  }
+//   render() {
+//     return (
+//       <div>
+//         <ul className="nav">
+//           <li onClick={() => this.props.handleClick("blog")}>
+//             <a href="#" id="special">
+//               {" "}
+//               PORTFOLIO
+//             </a>
+//           </li>
 
-  render() {
-    return (
-      <div>
-        <ul className="nav">
-          <li onClick={() => this.props.handleClick("blog")}>
-            <a href="#" id="special">
-              {" "}
-              PORTFOLIO
-            </a>
-          </li>
+//           <li onClick={() => this.props.handleClick("contact")}>
+//             <a href="#" id="special">
+//               CONTACT
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-          <li onClick={() => this.props.handleClick("contact")}>
-            <a href="#" id="special">
-              CONTACT
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default NavBar;
+// export default NavBar;
+const NavBar = () => {};
